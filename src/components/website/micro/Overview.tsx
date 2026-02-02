6412d870ffff35052cca851e3b96aae831c9bd31
Kuldeep Chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2026-02-02 18:10:57 +0530

    overview

--- src/components/website/micro/Overview.tsx
+++ src/components/website/micro/Overview.tsx
@@ -37,25 +37,25 @@ useEffect(() => {
 
   return (
     <section ref={sectionRef} className='py-[50px] md:py-[100px] bg-[#FEF7F0]'>
-        <div className='px-[7%] grid grid-cols-12 md:gap-[100px]'>
-          <div className="md:col-span-"></div>
-        <div className='md:col-span-6'>
+      <div className='px-[7%] grid grid-cols-12 md:gap-[100px]'>
+        <div className="md:col-span-1"></div>
+        <div className='col-span-12 md:col-span-6'>
           <div className='flex flex-col items-center justify-center h-full'>
-            
-          <h2 className="overview-animate w-full font-montserrat text-[32px] leading-[50px] font-normal tracking-[1px] capitalize text-[#F0801B] text-center">
-            Home to India’s Most Iconic Residential Lifestyle Experience
-          </h2>
 
-          <p className="w-full overview-animate font-montserrat my-[50px] text-[16px] leading-[25px] font-normal tracking-[1px] text-black text-center">
-           Edmont by Ashwin Sheth Group is a landmark luxury development in Kandivali West, spread across 2.02 acres with three 51-storey towers. Designed for refined living, it offers elegantly crafted 2 & 3 BHK residences with sweeping views of Manori Creek and lush greens. Blending contemporary design with future-ready luxury, Edmont delivers a sophisticated lifestyle at one of Mumbai’s most coveted addresses. </p>
-          <button onClick={() => setOpen(true)} className="overview-animate font-montserrat text-[18px] pb-[5px] font-medium tracking-[1px] border-b border-[#0E4194] uppercase text-[#0E4194] text-center block w-fit mx-auto">
-            Download Brochure
-          </button>
+            <h2 className="overview-animate w-full font-montserrat text-[32px] leading-[50px] font-normal tracking-[1px] capitalize text-[#F0801B] text-center">
+              Home to India’s Most Iconic Residential Lifestyle Experience
+            </h2>
+
+            <p className="w-full overview-animate font-montserrat my-[50px] text-[16px] leading-[25px] font-normal tracking-[1px] text-black text-center">
+              Edmont by Ashwin Sheth Group is a landmark luxury development in Kandivali West, spread across 2.02 acres with three 51-storey towers. Designed for refined living, it offers elegantly crafted 2 & 3 BHK residences with sweeping views of Manori Creek and lush greens. Blending contemporary design with future-ready luxury, Edmont delivers a sophisticated lifestyle at one of Mumbai’s most coveted addresses. </p>
+            <button onClick={() => setOpen(true)} className="overview-animate font-montserrat text-[18px] pb-[5px] font-medium tracking-[1px] border-b border-[#0E4194] uppercase text-[#0E4194] text-center block w-fit mx-auto">
+              Download Brochure
+            </button>
           </div>
         </div>
-<div className='md:col-span-5'>
-  <img src="/images/micro/overview.jpg" alt="overview" className='h-[500px] object-contain'/>
-</div>
+        <div className='col-span-12 md:col-span-5'>
+          <img src="/images/micro/overview.jpg" alt="overview" className='h-[500px] object-contain' />
+        </div>
 
       </div>
       <EnquiryForm open={open} onClose={() => setOpen(false)} />
